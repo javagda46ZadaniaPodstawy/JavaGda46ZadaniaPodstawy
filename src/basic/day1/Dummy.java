da18a922bf4e5607310d5bb33ad660a7b36af99e
Pawel Dudko <paweldudko@outlook.com>
2020-09-20 09:39:00 +0200

    extra task

---
+++ src/basic/day1/Dummy.java
@@ -0,0 +1,13 @@
+package basic.day1;
+
+public class Dummy {
+
+    public static void main(String[] args) {
+        for (int j = 1; j <= 10; j++) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.print(j * i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
