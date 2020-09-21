6c9eb84699ed1a25c0d1bbfb262f17077469ba8e
Pawel Dudko <paweldudko@outlook.com>
2020-09-21 11:45:05 +0200

    add tasks from day2

--- src/basic/extras/Ex1.java
+++ src/basic/extras/Ex1.java
@@ -1,11 +1,11 @@
-package basic.day1;
+package basic.extras;
 
-public class Dummy {
+public class Ex1 {
 
     public static void main(String[] args) {
         for (int j = 1; j <= 10; j++) {
             for (int i = 1; i <= 10; i++) {
-                System.out.print(j * i + " ");
+                System.out.print(j * i + "\t");
             }
             System.out.println();
         }
